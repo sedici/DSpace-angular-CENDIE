ce3383115204944b39c6cee206da619e3572d444
178Pelado <mosqueirafelipe22@gmail.com>
2026-08-13 14:17:26 -0300

    Agrego subtítulo y autor institucional. Tanto a la vista del ítem, como a los listados de ítems

--- src/themes/dspace/eager-theme.module.ts
+++ src/themes/dspace/eager-theme.module.ts
@@ -5,13 +5,18 @@ import { RootModule } from '../../app/root.module';
 import { HeaderComponent } from './app/header/header.component';
 import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
 import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
+import { UntypedItemComponent } from './app/item-page/simple/item-types/untyped-item/untyped-item.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
+import { ItemSearchResultListElementComponent } from './app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
  */
-const ENTRY_COMPONENTS = [];
+const ENTRY_COMPONENTS = [
+  UntypedItemComponent,
+  ItemSearchResultListElementComponent,
+];
 
 const DECLARATIONS = [
   ...ENTRY_COMPONENTS,
